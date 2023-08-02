04a43d306de8e0880616801917a6b8b80b060c52
rusmos <rusmos@gmail.com>
2023-08-02 19:06:00 +0300

    APP edit

--- src/App.js
+++ src/App.js
@@ -114,8 +114,8 @@ function App() {
         {tenzies && <Confetti />}
         <h1 className="title">Tenzies</h1>
         <p className="instructions">
-          Roll until all dice are the same. Click each die to freeze it at its
-          current value between rolls.
+          <p>Roll until all dice are the same.</p>
+          <p>Click each die to freeze it at its current value between rolls.</p>
         </p>
         <h2 className="track-rolls">Number of Rolls: {numOfRolls}</h2>
         <div className="timer">
